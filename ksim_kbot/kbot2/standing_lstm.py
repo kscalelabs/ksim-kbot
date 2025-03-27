e73869f032ceb8c74d570ad215c76cc67e1696a3
budzianowski <budzianowski@gmail.com>
2025-03-27 13:24:36 -0700

    fix other policies

--- ksim_kbot/kbot2/standing_lstm.py
+++ ksim_kbot/kbot2/standing_lstm.py
@@ -376,7 +376,7 @@ if __name__ == "__main__":
     KbotStandingLSTMTask.launch(
         KbotStandingLSTMTaskConfig(
             num_envs=2048,
-            num_batches=64,
+            batch_size=256,
             num_passes=10,
             # Simulation parameters.
             dt=0.002,
