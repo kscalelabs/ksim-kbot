fixed bug; was imu double scaling

ksim_kbot/deploy/deploy.py
@@ -143,7 +143,7 @@ class Deploy(ABC):
         elif self.mode == "sim":
             logger.debug(f"Resetting to: {self.default_positions_deg}")
 
-            #! In original script, the joint resets are diff
+            #! In original script, reset was using joints in kos.sim.reset() does not work
             await self.kos.sim.reset(
                 pos={"x": 0.0, "y": 0.0, "z": 1.01},
                 quat={"x": 0.0, "y": 0.0, "z": 0.0, "w": 1.0},
@@ -228,8 +228,8 @@ class Deploy(ABC):
                 action = np.array(self.model.infer(observation)).reshape(-1) 
 
                 #! Only Scale Action on observation but not onto Default Positions
-                position = action[: len(self.actuator_list)] + self.default_positions_rad
-                velocity = action[len(self.actuator_list) :]
+                position = action[: len(self.actuator_list)] * self.ACTION_SCALE + self.default_positions_rad
+                velocity = action[len(self.actuator_list) :] * self.ACTION_SCALE
                 
                 observation, _ = await asyncio.gather(
                     self.get_observation(),

ksim_kbot/deploy/deploy_joystick.py
@@ -84,10 +84,7 @@ class JoystickDeploy(Deploy):
             self.kos.imu.get_imu_values(),
         )
 
-        accel = np.array([imu.accel_x, imu.accel_y, imu.accel_z]) * self.GRAVITY
-        gyro = np.deg2rad(np.array([imu.gyro_x, imu.gyro_y, imu.gyro_z]))
-        imu_obs = np.concatenate([accel, gyro], axis=-1)
-        # imu_obs = np.array([0.0, 9.81, 0.0, 0.0, 0.0, 0.0])
+        imu_obs = np.array([imu.accel_x, imu.accel_y, imu.accel_z, imu.gyro_x, imu.gyro_y, imu.gyro_z])
 
         #* Pos Diff. Difference of current position from default position
         state_dict_pos = {state.actuator_id: state.position for state in actuator_states.states}
@@ -123,6 +120,7 @@ class JoystickDeploy(Deploy):
         return observation
 
 
+#* python -m ksim_kbot.deploy.deploy_joystick --model_path mlp_example --mode sim --scale_action 0.5 --debug
 def main():
     """Parse arguments and run the deploy script."""
     parser = argparse.ArgumentParser(description="Deploy a SavedModel on K-Bot")