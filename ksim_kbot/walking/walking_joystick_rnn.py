0338f4041e5f153f253c0bfe7b7e28eac867c441
budzianowski <budzianowski@gmail.com>
2025-04-12 23:03:26 -0700

    test rnn performance

---
+++ ksim_kbot/walking/walking_joystick_rnn.py
@@ -0,0 +1,426 @@
+# mypy: ignore-errors
+"""Defines simple task for training a standing policy for K-Bot."""
+
+from dataclasses import dataclass
+from typing import Generic, TypeVar
+
+import distrax
+import equinox as eqx
+import jax
+import jax.numpy as jnp
+import ksim
+import xax
+from jaxtyping import Array, PRNGKeyArray
+
+from ksim_kbot.walking.walking_joystick import (
+    NUM_INPUTS,
+    NUM_OUTPUTS,
+    KbotWalkingTask,
+    KbotWalkingTaskConfig,
+)
+
+NUM_INPUTS_CRITIC = NUM_INPUTS + 2 + 6 + 3 + 3 + 4 + 3 + 3 + 20 + 1
+
+
+class RnnActor(eqx.Module):
+    """RNN-based actor for the walking task."""
+
+    input_proj: eqx.nn.Linear
+    rnns: tuple[eqx.nn.GRUCell, ...]
+    output_proj: eqx.nn.Linear
+    num_inputs: int = eqx.static_field()
+    num_outputs: int = eqx.static_field()
+    min_std: float = eqx.static_field()
+    max_std: float = eqx.static_field()
+    var_scale: float = eqx.static_field()
+
+    def __init__(
+        self,
+        key: PRNGKeyArray,
+        *,
+        num_inputs: int,
+        num_outputs: int,
+        min_std: float,
+        max_std: float,
+        var_scale: float,
+        hidden_size: int,
+        depth: int,
+    ) -> None:
+        # Project input to hidden size
+        key, input_proj_key = jax.random.split(key)
+        self.input_proj = eqx.nn.Linear(
+            in_features=num_inputs,
+            out_features=hidden_size,
+            key=input_proj_key,
+        )
+
+        # Create RNN layer
+        key, rnn_key = jax.random.split(key)
+        self.rnns = tuple(
+            [
+                eqx.nn.GRUCell(
+                    input_size=hidden_size,
+                    hidden_size=hidden_size,
+                    key=rnn_key,
+                )
+                for _ in range(depth)
+            ]
+        )
+
+        # Project to output
+        self.output_proj = eqx.nn.Linear(
+            in_features=hidden_size,
+            out_features=num_outputs * 2,
+            key=key,
+        )
+
+        self.num_inputs = num_inputs
+        self.num_outputs = num_outputs
+        self.min_std = min_std
+        self.max_std = max_std
+        self.var_scale = var_scale
+
+    def forward(self, obs_n: Array, carry: Array) -> tuple[distrax.Distribution, Array]:
+        x_n = self.input_proj(obs_n)
+        out_carries = []
+        for i, rnn in enumerate(self.rnns):
+            x_n = rnn(x_n, carry[i])
+            out_carries.append(x_n)
+        out_n = self.output_proj(x_n)
+
+        # Converts the output to a distribution.
+        mean_n = out_n[..., : self.num_outputs]
+        std_n = out_n[..., self.num_outputs :]
+
+        # Softplus and clip to ensure positive standard deviations.
+        std_n = jnp.clip((jax.nn.softplus(std_n) + self.min_std) * self.var_scale, max=self.max_std)
+
+        dist_n = distrax.Normal(mean_n, std_n)
+        return dist_n, jnp.stack(out_carries, axis=0)
+
+
+class RnnCritic(eqx.Module):
+    """RNN-based critic for the walking task."""
+
+    input_proj: eqx.nn.Linear
+    rnns: tuple[eqx.nn.GRUCell, ...]
+    output_proj: eqx.nn.Linear
+
+    def __init__(
+        self,
+        key: PRNGKeyArray,
+        *,
+        num_inputs: int,
+        hidden_size: int,
+        depth: int,
+    ) -> None:
+        num_outputs = 1
+
+        # Project input to hidden size
+        key, input_proj_key = jax.random.split(key)
+        self.input_proj = eqx.nn.Linear(
+            in_features=num_inputs,
+            out_features=hidden_size,
+            key=input_proj_key,
+        )
+
+        # Create RNN layer
+        key, rnn_key = jax.random.split(key)
+        self.rnns = tuple(
+            [
+                eqx.nn.GRUCell(
+                    input_size=hidden_size,
+                    hidden_size=hidden_size,
+                    key=rnn_key,
+                )
+                for _ in range(depth)
+            ]
+        )
+
+        # Project to output
+        self.output_proj = eqx.nn.Linear(
+            in_features=hidden_size,
+            out_features=num_outputs,
+            key=key,
+        )
+
+    def forward(self, obs_n: Array, carry: Array) -> tuple[Array, Array]:
+        x_n = self.input_proj(obs_n)
+        out_carries = []
+        for i, rnn in enumerate(self.rnns):
+            x_n = rnn(x_n, carry[i])
+            out_carries.append(x_n)
+        out_n = self.output_proj(x_n)
+
+        return out_n, jnp.stack(out_carries, axis=0)
+
+
+class RnnModel(eqx.Module):
+    actor: RnnActor
+    critic: RnnCritic
+
+    def __init__(
+        self,
+        key: PRNGKeyArray,
+        *,
+        min_std: float,
+        max_std: float,
+        num_inputs: int,
+        num_inputs_critic: int,
+        num_joints: int,
+        hidden_size: int,
+        depth: int,
+    ) -> None:
+        self.actor = RnnActor(
+            key,
+            num_inputs=num_inputs,
+            num_outputs=num_joints,
+            min_std=min_std,
+            max_std=max_std,
+            var_scale=0.5,
+            hidden_size=hidden_size,
+            depth=depth,
+        )
+        self.critic = RnnCritic(
+            key,
+            num_inputs=num_inputs_critic,
+            hidden_size=hidden_size,
+            depth=depth,
+        )
+
+
+@dataclass
+class KbotRnnWalkingTaskConfig(KbotWalkingTaskConfig):
+    # Model parameters.
+    hidden_size: int = xax.field(
+        value=128,
+        help="The hidden size for the MLPs.",
+    )
+    depth: int = xax.field(
+        value=5,
+        help="The depth for the MLPs.",
+    )
+
+
+Config = TypeVar("Config", bound=KbotRnnWalkingTaskConfig)
+
+
+class KbotRnnWalkingTask(KbotWalkingTask[Config], Generic[Config]):
+    config: Config
+
+    def get_model(self, key: PRNGKeyArray) -> RnnModel:
+        return RnnModel(
+            key,
+            num_inputs=NUM_INPUTS,
+            num_joints=NUM_OUTPUTS,
+            num_inputs_critic=NUM_INPUTS_CRITIC,
+            min_std=0.01,
+            max_std=1.0,
+            hidden_size=self.config.hidden_size,
+            depth=self.config.depth,
+        )
+
+    def run_actor(
+        self,
+        model: RnnActor,
+        observations: xax.FrozenDict[str, Array],
+        commands: xax.FrozenDict[str, Array],
+        carry: Array,
+    ) -> tuple[distrax.Distribution, Array]:
+        timestep_phase_4 = observations["timestep_phase_observation"]
+        joint_pos_n = observations["joint_position_observation"]
+        joint_vel_n = observations["joint_velocity_observation"]
+        imu_acc_3 = observations["sensor_observation_imu_acc"]
+        imu_gyro_3 = observations["sensor_observation_imu_gyro"]
+        lin_vel_cmd_2 = commands["linear_velocity_command"]
+        ang_vel_cmd = commands["angular_velocity_command"]
+        gait_freq_cmd = commands["gait_frequency_command"]
+        last_action_n = observations["last_action_observation"]
+        obs_n = jnp.concatenate(
+            [
+                timestep_phase_4,
+                joint_pos_n,
+                joint_vel_n,
+                imu_acc_3,
+                imu_gyro_3,
+                lin_vel_cmd_2,
+                ang_vel_cmd,
+                gait_freq_cmd,
+                last_action_n,
+            ],
+            axis=-1,
+        )
+
+        return model.forward(obs_n, carry)
+
+    def run_critic(
+        self,
+        model: RnnCritic,
+        observations: xax.FrozenDict[str, Array],
+        commands: xax.FrozenDict[str, Array],
+        carry: Array,
+    ) -> Array:
+        timestep_phase_4 = observations["timestep_phase_observation"]
+        joint_pos_n = observations["joint_position_observation"]
+        joint_vel_n = observations["joint_velocity_observation"]
+        imu_acc_3 = observations["sensor_observation_imu_acc"]
+        imu_gyro_3 = observations["sensor_observation_imu_gyro"]
+        projected_gravity_3 = observations["projected_gravity_observation"]
+        lin_vel_cmd_2 = commands["linear_velocity_command"]
+        ang_vel_cmd = commands["angular_velocity_command"]
+        gait_freq_cmd = commands["gait_frequency_command"]
+        last_action_n = observations["last_action_observation"]
+        # critic observations
+        feet_contact_2 = observations["feet_contact_observation"]
+        feet_position_6 = observations["feet_position_observation"]
+        base_position_3 = observations["base_position_observation"]
+        base_orientation_4 = observations["base_orientation_observation"]
+        base_linear_velocity_3 = observations["base_linear_velocity_observation"]
+        base_angular_velocity_3 = observations["base_angular_velocity_observation"]
+        actuator_force_n = observations["actuator_force_observation"]
+        true_height_1 = observations["true_height_observation"]
+
+        obs_n = jnp.concatenate(
+            [
+                timestep_phase_4,
+                joint_pos_n,
+                joint_vel_n,
+                imu_acc_3,
+                imu_gyro_3,
+                lin_vel_cmd_2,
+                ang_vel_cmd,
+                gait_freq_cmd,
+                last_action_n,
+                # critic observations
+                feet_contact_2,
+                feet_position_6,
+                projected_gravity_3,
+                base_position_3,
+                base_orientation_4,
+                base_linear_velocity_3,
+                base_angular_velocity_3,
+                actuator_force_n,
+                true_height_1,
+            ],
+            axis=-1,
+        )
+
+        return model.forward(obs_n, carry)
+
+    def get_initial_model_carry(self, rng: PRNGKeyArray) -> tuple[Array, Array]:
+        return (
+            jnp.zeros(shape=(self.config.depth, self.config.hidden_size)),
+            jnp.zeros(shape=(self.config.depth, self.config.hidden_size)),
+        )
+
+    def get_ppo_variables(
+        self,
+        model: RnnModel,
+        trajectory: ksim.Trajectory,
+        model_carry: tuple[Array, Array],
+        rng: PRNGKeyArray,
+    ) -> tuple[ksim.PPOVariables, tuple[Array, Array]]:
+        def scan_fn(
+            actor_critic_carry: tuple[Array, Array], transition: ksim.Trajectory
+        ) -> tuple[tuple[Array, Array], ksim.PPOVariables]:
+            actor_carry, critic_carry = actor_critic_carry
+            actor_dist, next_actor_carry = self.run_actor(
+                model=model.actor,
+                observations=transition.obs,
+                commands=transition.command,
+                carry=actor_carry,
+            )
+            log_probs = actor_dist.log_prob(transition.action)
+            assert isinstance(log_probs, Array)
+            value, next_critic_carry = self.run_critic(
+                model=model.critic,
+                observations=transition.obs,
+                commands=transition.command,
+                carry=critic_carry,
+            )
+
+            transition_ppo_variables = ksim.PPOVariables(
+                log_probs=log_probs,
+                values=value.squeeze(-1),
+            )
+
+            initial_carry = self.get_initial_model_carry(rng)
+            next_carry = jax.tree.map(
+                lambda x, y: jnp.where(transition.done, x, y), initial_carry, (next_actor_carry, next_critic_carry)
+            )
+
+            return next_carry, transition_ppo_variables
+
+        next_model_carry, ppo_variables = jax.lax.scan(scan_fn, model_carry, trajectory)
+
+        return ppo_variables, next_model_carry
+
+    def sample_action(
+        self,
+        model: RnnModel,
+        model_carry: tuple[Array, Array],
+        physics_model: ksim.PhysicsModel,
+        physics_state: ksim.PhysicsState,
+        observations: xax.FrozenDict[str, Array],
+        commands: xax.FrozenDict[str, Array],
+        rng: PRNGKeyArray,
+    ) -> ksim.Action:
+        actor_carry_in, critic_carry_in = model_carry
+
+        # Runs the actor model to get the action distribution.
+        action_dist_j, actor_carry = self.run_actor(
+            model=model.actor,
+            observations=observations,
+            commands=commands,
+            carry=actor_carry_in,
+        )
+
+        action_j = action_dist_j.sample(seed=rng)
+
+        return ksim.Action(
+            action=action_j,
+            carry=(actor_carry, critic_carry_in),
+            aux_outputs=None,
+        )
+
+
+if __name__ == "__main__":
+    # python -m ksim_kbot.walking.walking_joystick_rnn num_envs=2 batch_size=2
+    # To run training, use the following command:
+    # python -m ksim_kbot.walking.walking_joystick_rnn.py disable_multiprocessing=True
+    # To visualize the environment, use the following command:
+    # python -m ksim_kbot.walking.walking_joystick_rnn.py run_environment=True \
+    #  run_environment_num_seconds=1 \
+    #  run_environment_save_path=videos/test.mp4
+    KbotRnnWalkingTask.launch(
+        KbotRnnWalkingTaskConfig(
+            num_envs=2048,
+            batch_size=256,
+            num_passes=10,
+            epochs_per_log_step=1,
+            # Simulation parameters.
+            dt=0.002,
+            ctrl_dt=0.02,
+            max_action_latency=0.0,
+            min_action_latency=0.0,
+            rollout_length_seconds=5.0,
+            # PPO parameters
+            action_scale=1.0,
+            gamma=0.97,
+            lam=0.95,
+            entropy_coef=0.005,
+            learning_rate=1e-4,
+            clip_param=0.3,
+            max_grad_norm=0.5,
+            log_full_trajectory_every_n_steps=5,
+            save_every_n_steps=25,
+            export_for_inference=True,
+            only_save_most_recent=False,
+            # Task parameters
+            domain_randomize=True,
+            gait_freq_lower=1.25,
+            gait_freq_upper=1.5,
+            reward_clip_min=0.0,
+            reward_clip_max=1000.0,
+        ),
+    )
